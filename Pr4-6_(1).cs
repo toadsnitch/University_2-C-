f5b254784e5eef3f9f23eaf5017ef5b91e693779
toadsnitch <116301034+toadsnitch@users.noreply.github.com>
2023-09-24 14:00:50 +0300

    Create Pr4-6_(1).cs

---
+++ Pr4-6_(1).cs
@@ -0,0 +1,24 @@
+using System;
+
+
+namespace Total
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 2;
+            Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());
+            
+            for(int i = 2; i <= a / 2 + 1; i++) {
+                if (a % i == 0) count++;
+            }
+
+            Console.WriteLine(count * 2);
+
+
+
+        }
+    }
+}
