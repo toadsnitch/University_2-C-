227f2e327cfc33da9ebce8ea605f67809c2a5429
toadsnitch <116301034+toadsnitch@users.noreply.github.com>
2024-05-12 21:00:29 +0300

    Create Program.cs

---
+++ 21 : 3__(1)/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace BinTree3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader fileIn = new StreamReader("input.txt"))
+            {
+                BinaryTree tree = new BinaryTree(); //Создаем дерево
+
+                string line;
+                char[] div = { ' ' };
+                while ((line = fileIn.ReadLine()) != null)
+                {
+                    string[] mas = line.Split(div, StringSplitOptions.RemoveEmptyEntries);
+                    for (int i = 0; i < mas.Length; i++)
+                    {
+                        tree.Add(int.Parse(mas[i]));
+                    }
+                }
+
+                if (tree.IsBalanced())
+                {
+                    Console.WriteLine("The tree is balanced\n");
+                    tree.Preorder();
+                }
+                else
+                {
+                    Console.WriteLine("The tree isn't balanced\nBalanced Tree:\n");
+                    tree.Balancer();
+                    tree.Preorder();
+                }          
+            }
+        }
+    }
+}
